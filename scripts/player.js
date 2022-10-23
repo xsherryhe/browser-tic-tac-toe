9f244cea35535f843f92545b5ebd1f44a345314b
Sherry He <xsherryhe@gmail.com>
2022-10-23 01:18:02 -0400

    Added max by average secondary to minimax to refine computer player algorithm

--- scripts/player.js
+++ scripts/player.js
@@ -28,27 +28,27 @@ function ComputerPlayer(marker) {
   }
 
   function _selectOptimizedSquare(board) {
-    const indexesWithBoardValues = 
-      board.availableIndexes().map(index => {
-        return { index, boardVal: _boardValue(board, index, board.markers.indexOf(marker)) };
-      })
-    return indexesWithBoardValues.sort((a, b) => b.boardVal - a.boardVal)[0].index;
+    const indexesWithBoardValues = board.availableIndexes().map(index =>
+      Object.assign({ index }, _boardValues(board, index, board.markers.indexOf(marker))));
+    return indexesWithBoardValues.sort((a, b) => 
+      b.optimizedVal - a.optimizedVal || b.averageVal - a.averageVal)[0].index;
   }
 
-  function _boardValue(board, index, currMarkerInd) {
+  function _boardValues(board, index, currMarkerInd) {
     const nextBoard = Board(board.markers, [...board.state.map(row => [...row])]);
     nextBoard.setSquare(index, board.markers[currMarkerInd]);
 
-    if (gameConditions.win(marker, nextBoard)) return 1;
-    if (gameConditions.lose(marker, nextBoard)) return -1;
-    if (gameConditions.tie(nextBoard)) return 0;
+    if (gameConditions.win(marker, nextBoard)) return { optimizedVal: 1, averageVal: 1 };
+    if (gameConditions.lose(marker, nextBoard)) return { optimizedVal: -1, averageVal: -1 };
+    if (gameConditions.tie(nextBoard)) return { optimizedVal: 0, averageVal: 0 };
 
     const nextMarkerInd = currMarkerInd ^ 1,
-          nextBoardValues = nextBoard.availableIndexes().map(nextIndex => _boardValue(nextBoard, nextIndex, nextMarkerInd));
-    //normal mode
-    //return nextBoardValues.reduce((a, b) => a + b, 0) / nextBoardValues.length;
-    //hard mode
-    return board.markers[nextMarkerInd] == marker ? Math.max(...nextBoardValues) : Math.min(...nextBoardValues);
+          nextBoardValues = nextBoard.availableIndexes().map(nextIndex => _boardValues(nextBoard, nextIndex, nextMarkerInd)),
+          nextBoardOptimizedValues = nextBoardValues.map(values => values.optimizedVal),
+          optimizedVal = board.markers[nextMarkerInd] == marker ? Math.max(...nextBoardOptimizedValues) : Math.min(...nextBoardOptimizedValues),
+          averageVal = nextBoardValues.map(values => values.averageVal).reduce((a, b) => a + b, 0) / nextBoardValues.length;
+
+    return { optimizedVal, averageVal };
   }
 
   return Object.assign({ type: 'computer', name, setName, messageRecipient, turnMessage, selectSquare }, prototype);
